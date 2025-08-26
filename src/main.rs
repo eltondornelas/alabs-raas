a79d020856ab3a06753cb54d4eed1df9adc652ea
Elton Dornelas <eltondornelas@gmail.com>
2025-08-26 07:19:52 -0400

    including routes for extraction examples

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::collections::HashMap;
+
+use axum::extract::Path;
+use axum::extract::Query;
+use axum::http::HeaderMap;
 use axum::{Router, response::Html, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/book/{id}", get(path_extract))
+        .route("/book", get(query_extract))
+        .route("/header", get(header_extract));
 
     // axum do anything if not talking to the network
     // axum uses tokio to handle connectivity
@@ -17,4 +26,19 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello World</h1>")
+}
+
+async fn path_extract(Path(id): Path<u32>) -> Html<String> {
+    Html(format!("Hello, {id}!"))
+}
+
+async fn query_extract(Query(params): Query<HashMap<String, String>>) -> Html<String> {
+    Html(format!("Hello, {params:#?}!"))
+}
+
+async fn header_extract(
+    headers: HeaderMap,
+    // is not really a extractor
+) -> Html<String> {
+    Html(format!("{headers:#?}"))
 }
