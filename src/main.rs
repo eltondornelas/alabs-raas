e53f55b36aef78965afcbabecbd3473bbbbe6944
Elton Dornelas <eltondornelas@gmail.com>
2025-08-26 07:36:34 -0400

    adding a immutable State

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,27 @@ use std::collections::HashMap;
 
 use axum::extract::Path;
 use axum::extract::Query;
+use axum::extract::State;
 use axum::http::HeaderMap;
 use axum::{Router, response::Html, routing::get};
+use std::sync::Arc; // common wrapper for State because most of the time the State will be cloned into a handler function
+
+struct MyConfig {
+    config_string: String,
+}
 
 #[tokio::main]
 async fn main() {
+    let shared_config = Arc::new(MyConfig {
+        config_string: "My config string".to_string(),
+    });
+
     let app = Router::new()
         .route("/", get(handler))
         .route("/book/{id}", get(path_extract))
         .route("/book", get(query_extract))
-        .route("/header", get(header_extract));
+        .route("/header", get(header_extract))
+        .with_state(shared_config);
 
     // axum do anything if not talking to the network
     // axum uses tokio to handle connectivity
@@ -24,8 +35,11 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello World</h1>")
+async fn handler(
+    State(config): State<Arc<MyConfig>>,
+    // destructuring
+) -> Html<String> {
+    Html(format!("<h1>{}</h1>", config.config_string))
 }
 
 async fn path_extract(Path(id): Path<u32>) -> Html<String> {
