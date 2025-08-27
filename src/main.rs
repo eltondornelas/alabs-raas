031d0cc75a1a5143d9727fa57bb4983faa24e2b0
Elton Dornelas <eltondornelas@gmail.com>
2025-08-26 20:46:27 -0400

    adding multiple states with Extension

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
 use std::collections::HashMap;
 use std::sync::atomic::AtomicUsize;
 
+use axum::Extension; // extensions have to be clonable
 use axum::extract::Path;
 use axum::extract::Query;
-use axum::extract::State;
+// use axum::extract::State;
 use axum::http::HeaderMap;
 use axum::{Router, response::Html, routing::get};
 use std::sync::Arc; // common wrapper for State because most of the time the State will be cloned into a handler function
 
-struct MyConfig {
+struct MyCounter {
     counter: AtomicUsize,
     // note: if using Mutex to lock some data, make sure to use wisely and not Mutex the root data or global data or you would be locking the whole handler having to wait
 }
 
+struct MyConfig {
+    text: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let shared_config = Arc::new(MyConfig {
+    let shared_counter = Arc::new(MyCounter {
         counter: AtomicUsize::new(0),
+    });
+
+    let shared_text = Arc::new(MyConfig {
+        text: "This is my configuration".to_string(),
     });
 
     let app = Router::new()
@@ -24,7 +33,9 @@ async fn main() {
         .route("/book/{id}", get(path_extract))
         .route("/book", get(query_extract))
         .route("/header", get(header_extract))
-        .with_state(shared_config);
+        .layer(Extension(shared_text))
+        .layer(Extension(shared_counter)); // layer system is ideal to use with database connection
+    // .with_state(shared_config);
 
     // axum do anything if not talking to the network
     // axum uses tokio to handle connectivity
@@ -38,16 +49,17 @@ async fn main() {
 }
 
 async fn handler(
-    State(config): State<Arc<MyConfig>>,
-    // destructuring
+    Extension(counter): Extension<Arc<MyCounter>>,
+    Extension(config): Extension<Arc<MyConfig>>, // destructuring
 ) -> Html<String> {
-    config
+    counter
         .counter
         .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-    
+
     Html(format!(
-        "<h1>Your are visitor number {}</h1>",
-        config.counter.load(std::sync::atomic::Ordering::Relaxed)
+        "<h1>{} Your are visitor number {}</h1>",
+        config.text,
+        counter.counter.load(std::sync::atomic::Ordering::Relaxed)
     ))
 }
 
