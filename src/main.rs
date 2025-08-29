62026d01b96df01dbaea44444229873b082ecc07
Elton Dornelas <eltondornelas@gmail.com>
2025-08-28 20:46:58 -0400

    sending and receiving header

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,49 @@
-use axum::{response::{Html, IntoResponse}, routing::get, Router};
+use std::time::Duration;
+
+use axum::{
+    Router,
+    http::HeaderMap,
+    response::{Html, IntoResponse},
+    routing::get,
+};
 use reqwest::StatusCode;
-use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(handler))
-        .fallback_service(ServeDir::new("web"));
+    let app = Router::new().route("/", get(header_handler));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3001")
         .await
         .unwrap();
 
+    tokio::spawn(make_request());
+
     println!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> Result<impl IntoResponse, StatusCode> {
-    Ok(Html("<h1>Hello, World!</h1>"))
+async fn make_request() {
+    // Pause to let the server start up
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    // Make a request to the server
+    let response = reqwest::Client::new()
+        .get("http://localhost:3001/")
+        .header("x-request-id", "1234")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    println!("{}", response);
+}
+
+async fn header_handler(headers: HeaderMap) -> Html<String> {
+    if let Some(header) = headers.get("x-request-id") {
+        Html(format!("x-request-id: {}", header.to_str().unwrap()))
+    } else {
+        Html("x-request-id not found".to_string())
+    }
 }
