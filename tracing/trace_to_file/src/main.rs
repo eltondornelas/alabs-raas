7ef13d9df98785e4798d33b53102636a2e238133
Elton Dornelas <eltondornelas@gmail.com>
2025-08-31 09:49:51 -0400

    tracing - logging to a file as json

--- tracing/trace_to_file/src/main.rs
+++ tracing/trace_to_file/src/main.rs
@@ -2,10 +2,12 @@ use tracing::info;
 
 fn main() {
     // Setup tracing
-    let file_appender = tracing_appender::rolling::hourly("test.log", "prefix.log"); // once per hour
+    // let file_appender = tracing_appender::rolling::hourly("test.log", "prefix.log"); // once per hour
+    let file_appender = tracing_appender::rolling::hourly("test_json.log", "prefix.log"); // once per hour
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
 
     tracing_subscriber::fmt()
+        .json()
         .with_writer(non_blocking)
         // Build the subscriber
         .init();
@@ -15,3 +17,5 @@ fn main() {
 
 // cargo add tracing_appender
 // cat test.log/prefix...
+
+// cargo add tracing_subscriber -F json
