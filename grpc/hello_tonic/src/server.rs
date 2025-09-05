861ba0b55a564d4668e00c6c09e6cc469948f18e
Elton Dornelas <eltondornelas@gmail.com>
2025-09-05 19:01:32 -0400

    grpc - creating a minimal grpc app with tonic

---
+++ grpc/hello_tonic/src/server.rs
@@ -0,0 +1,45 @@
+use tonic::{Request, Response, Status, transport::Server};
+
+// application name
+pub mod hello_world {
+    tonic::include_proto!("hello");
+    // important to separate in modules otherwise you gonna create a big namespace
+}
+
+use hello_world::greeter_server::{Greeter, GreeterServer};
+use hello_world::{HelloReply, HelloRequest};
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = hello_world::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
+
+// cargo run --bin helloworld-server
+// cargo run --bin helloworld-client
